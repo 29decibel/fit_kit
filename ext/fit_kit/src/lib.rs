9830aa8484225986723b297d955c6e8676717a3b
Mike Li <m@idots.me>
2024-10-16 17:18:38 -0700

    Add couple of calculation methods (#3)
    * adding avg_for and elevation_gain method
    
    * add partition indices method
    
    * partition actual value
    
    * add partition_stats_for_fields

--- ext/fit_kit/src/lib.rs
+++ ext/fit_kit/src/lib.rs
@@ -1,7 +1,223 @@
-use fitparser::{self, FitDataRecord, Value};
+use fitparser::{self, profile::MesgNum, FitDataField, FitDataRecord, Value};
 use itertools::Itertools;
-use magnus::{function, prelude::*, Error, IntoValue, RArray, RHash, Ruby, Symbol};
+use magnus::{function, method, prelude::*, Error, IntoValue, RArray, RHash, Ruby, Symbol};
 use std::fs::File;
+
+// wrap fitparse value
+struct MyValue(Value);
+
+impl MyValue {
+    // turn value into f64
+    fn as_f64(&self) -> Option<f64> {
+        match &self.0 {
+            Value::SInt8(i) => Some(*i as f64),
+            Value::UInt8(u) => Some(*u as f64),
+            Value::SInt16(i) => Some(*i as f64),
+            Value::UInt16(u) => Some(*u as f64),
+            Value::SInt32(i) => Some(*i as f64),
+            Value::UInt32(u) => Some(*u as f64),
+            Value::Float32(f) => Some(*f as f64),
+            Value::Float64(f) => Some(*f),
+            Value::UInt8z(u) => Some(*u as f64),
+            Value::UInt16z(u) => Some(*u as f64),
+            Value::UInt32z(u) => Some(*u as f64),
+            Value::SInt64(i) => Some(*i as f64),
+            Value::UInt64(u) => Some(*u as f64),
+            Value::UInt64z(u) => Some(*u as f64),
+            _ => None, // Handle any other variants that don't convert to f64
+        }
+    }
+}
+
+#[magnus::wrap(class = "FitParseResult")]
+struct FitParseResult(Vec<FitDataRecord>);
+
+impl FitParseResult {
+    /**
+     * Returns Ruby hash for all the records
+     * With keys are the record types
+     */
+    fn records_hash(&self) -> RHash {
+        // now let's group by the record by kind
+        let result_hash = RHash::new();
+        for (kind, kind_records) in self
+            .0
+            .iter()
+            .chunk_by(|record| record.kind().to_string())
+            .into_iter()
+        {
+            // turn records into rarray
+            let array = RArray::new();
+            for record in kind_records {
+                // TODO here do not pass RFitDataRecord
+                // turn it into fields_hash directly
+                array.push(get_fields_hash(record)).unwrap();
+            }
+
+            result_hash.aset(Symbol::new(kind), array).unwrap();
+        }
+
+        result_hash
+    }
+
+    // summary methods
+    fn avg_for(&self, field_name: String) -> (f64, String) {
+        // passing the reference
+        self.avg_for_records(&self.0, field_name)
+    }
+
+    fn elevation_gain(&self, field_name: String) -> (f64, String) {
+        self.elevation_gain_for_records(&self.0, field_name)
+    }
+
+    // given a bunch of records, calculate the elevation gain
+    fn elevation_gain_for_records(
+        &self,
+        records: &Vec<FitDataRecord>,
+        field_name: String,
+    ) -> (f64, String) {
+        let fields: Vec<&FitDataField> = records
+            .iter()
+            .filter(|r| r.kind() == MesgNum::Record)
+            .flat_map(|r| r.fields().iter().filter(|field| field.name() == field_name))
+            .collect();
+
+        let count = fields.len();
+
+        if count == 0 {
+            return (0.0, String::from(""));
+        }
+
+        let units = fields.first().unwrap().units();
+
+        let elevation_gain_sum = fields.windows(2).fold(0.0, |acc, window| {
+            // find the field first
+            let value1 = MyValue(window[1].value().clone()).as_f64();
+            let value0 = MyValue(window[0].value().clone()).as_f64();
+
+            match (value1, value0) {
+                (Some(v1), Some(v0)) if v1 > v0 => acc + (v1 - v0),
+                _ => acc,
+            }
+        });
+
+        (elevation_gain_sum, String::from(units))
+    }
+
+    fn partition_stats_for_fields(
+        &self,
+        by_field: String,
+        partition_distance: f64,
+        fields: Vec<String>,
+    ) -> Vec<Vec<(String, (f64, String))>> {
+        // first get the partitions
+        let partition_indices = self.calculate_partition_indices(partition_distance, by_field);
+
+        partition_indices
+            .windows(2)
+            .map(|window| {
+                let start = window[0];
+                let end = window[1];
+                let partition_records = self.0[start..=end].to_vec();
+
+                // ok we have this, now we can map the resulf the field
+                let results: Vec<(String, (f64, String))> = fields
+                    .iter()
+                    .map(|field_name| {
+                        (
+                            field_name.clone(),
+                            self.avg_for_records(&partition_records, field_name.clone()),
+                        )
+                    })
+                    .collect();
+
+                results
+            })
+            .collect()
+    }
+
+    fn avg_for_records(&self, records: &Vec<FitDataRecord>, field_name: String) -> (f64, String) {
+        // only get the record types
+        let fields: Vec<&FitDataField> = records
+            .iter()
+            .filter(|r| r.kind() == MesgNum::Record)
+            .flat_map(|r| r.fields().iter().filter(|field| field.name() == field_name))
+            .collect();
+
+        // do a map filter to only sum the possible values could be sumed
+        // we only care about int, float values
+        let sumable_values: Vec<f64> = fields
+            .iter()
+            .filter_map(|field| MyValue(field.value().clone()).as_f64())
+            .collect();
+
+        let sum: f64 = sumable_values.iter().sum();
+        let count = sumable_values.len();
+
+        if count == 0 {
+            (0.0, String::from(""))
+        } else {
+            // we also need to return the unit
+            let units = fields.first().unwrap().units();
+            let avg_value = sum / count as f64;
+            (avg_value, String::from(units))
+        }
+    }
+
+    fn calculate_partition_indices(
+        &self,
+        partition_distance: f64,
+        field_name: String,
+    ) -> Vec<usize> {
+        let records: Vec<&FitDataRecord> = self
+            .0
+            .iter()
+            .filter(|r| r.kind() == MesgNum::Record)
+            .collect();
+        self.calculate_partition_indices_for_records(records, partition_distance, field_name)
+    }
+
+    fn calculate_partition_indices_for_records(
+        &self,
+        records: Vec<&FitDataRecord>,
+        partition_distance: f64,
+        field_name: String,
+    ) -> Vec<usize> {
+        let mut partition_indices = vec![0]; // always start include the start index
+        let mut start_distance = 0.0;
+
+        // let's loop
+        for (index, record) in records.iter().enumerate().skip(1) {
+            let fields: Vec<&FitDataField> = record
+                .fields()
+                .iter()
+                .filter(|f| f.name() == field_name)
+                .collect();
+
+            let distance_field = fields
+                .first()
+                .and_then(|f| MyValue(f.value().clone()).as_f64());
+            match distance_field {
+                Some(distance_value) => {
+                    if distance_value - start_distance >= partition_distance {
+                        // found it
+                        partition_indices.push(index);
+                        start_distance = distance_value;
+                    }
+                }
+                None => {}
+            }
+        }
+
+        // now we have the whole array
+        // if the last record is not there, add it
+        if *partition_indices.last().unwrap() != records.len() - 1 {
+            partition_indices.push(records.len() - 1);
+        }
+
+        partition_indices
+    }
+}
 
 // recursive method to turn Fit value into magnus::Value
 fn value_to_rb_value(value: &Value) -> magnus::Value {
@@ -34,6 +250,7 @@ fn value_to_rb_value(value: &Value) -> magnus::Value {
     }
 }
 
+// Turning FitDataRecord into a hash
 fn get_fields_hash(record: &FitDataRecord) -> RHash {
     let hash = RHash::new();
     for field in record.fields() {
@@ -49,7 +266,29 @@ fn get_fields_hash(record: &FitDataRecord) -> RHash {
     hash
 }
 
-fn parse_fit_file(file_path: String) -> Result<RHash, magnus::Error> {
+// Here we define two ruby classes
+// RFitDataRecord and RFitDataField
+fn define_ruby_classes(ruby: &Ruby) -> Result<(), magnus::Error> {
+    // definie the the other one here
+    let data_record_class = ruby.define_class("FitParseResult", ruby.class_object())?;
+    data_record_class.define_method("records_hash", method!(FitParseResult::records_hash, 0))?;
+    data_record_class.define_method("avg_for", method!(FitParseResult::avg_for, 1))?;
+    data_record_class
+        .define_method("elevation_gain", method!(FitParseResult::elevation_gain, 1))?;
+    data_record_class.define_method(
+        "calculate_partition_indices",
+        method!(FitParseResult::calculate_partition_indices, 2),
+    )?;
+
+    data_record_class.define_method(
+        "partition_stats_for_fields",
+        method!(FitParseResult::partition_stats_for_fields, 3),
+    )?;
+
+    Ok(())
+}
+
+fn parse_fit_file(file_path: String) -> Result<FitParseResult, magnus::Error> {
     let mut fp = File::open(file_path)
         .map_err(|e| Error::new(Ruby::get().unwrap().exception_io_error(), e.to_string()))?;
     let data = fitparser::from_reader(&mut fp).map_err(|e| {
@@ -59,31 +298,16 @@ fn parse_fit_file(file_path: String) -> Result<RHash, magnus::Error> {
         )
     })?;
 
-    // now let's group by the record by kind
-    let result_hash = RHash::new();
-    for (kind, kind_records) in data
-        .iter()
-        .chunk_by(|record| record.kind().to_string())
-        .into_iter()
-    {
-        // turn records into rarray
-        let array = RArray::new();
-        for record in kind_records {
-            // TODO here do not pass RFitDataRecord
-            // turn it into fields_hash directly
-            array.push(get_fields_hash(record)).unwrap();
-        }
+    let result = FitParseResult(data);
 
-        result_hash.aset(Symbol::new(kind), array).unwrap();
-    }
-
-    Ok(result_hash)
+    Ok(result)
 }
 
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let module = ruby.define_module("FitKit")?;
 
+    let _ = define_ruby_classes(ruby);
     module.define_singleton_method("parse_fit_file", function!(parse_fit_file, 1))?;
 
     Ok(())
--- test/test_fit_kit.rb
+++ test/test_fit_kit.rb
@@ -17,8 +17,7 @@ class TestFitKit < Minitest::Test
   end
 
   def test_parse_fit_file
-    pp fit_data_records.keys
-    assert_equal(fit_data_records.keys, [
+    assert_equal(fit_data_records.records_hash.keys, [
       :file_id,
       :developer_data_id,
       :device_info,
@@ -30,13 +29,13 @@ class TestFitKit < Minitest::Test
       :session
     ], "Fit file keys should match")
     # check the record type
-    assert_equal(fit_data_records[:record].size, 876, "Record kind items should match")
+    assert_equal(876, fit_data_records.records_hash[:record].size, "Record kind items should match")
   end
 
   def test_parse_fit_file_hash
     # pick some of them
-    fields_hash = fit_data_records[:record][123]
-    assert_equal(fields_hash, {position_lat: {units: "semicircles", value: 402434146},
+    fields_hash = fit_data_records.records_hash[:record][123]
+    assert_equal({position_lat: {units: "semicircles", value: 402434146},
      position_long: {units: "semicircles", value: -1404677685},
      heart_rate: {units: "bpm", value: 132},
      cadence: {units: "rpm", value: 89},
@@ -47,6 +46,47 @@ class TestFitKit < Minitest::Test
      enhanced_speed: {units: "m/s", value: 2.969},
      enhanced_altitude: {units: "m", value: 133.0},
      step_length: {units: "mm", value: 1000.0},
-     timestamp: {units: "s", value: 1624025048}})
+     timestamp: {units: "s", value: 1624025048}}, fields_hash)
+  end
+
+  def test_avg_for_method
+    assert_equal([133.0, "m"], fit_data_records.elevation_gain("enhanced_altitude"))
+  end
+
+  def test_elevation_gain_method
+    assert_equal([128.11962537249894, "bpm"], fit_data_records.avg_for("heart_rate"), "Avg heart rate should match")
+    assert_equal([89.60828705681332, "rpm"], fit_data_records.avg_for("cadence"), "Avg candence should match")
+    assert_equal([2.816479709525839, "m/s"], fit_data_records.avg_for("enhanced_speed"), "Avg speed should match")
+    assert_equal([210.02134016218523, "watts"], fit_data_records.avg_for("power"), "Avg power should match")
+  end
+
+  def test_partition_distance_records
+    partition_indices = fit_data_records.calculate_partition_indices(1600, "distance")
+    assert_equal([0, 575, 1104, 1693, 2281, 2354], partition_indices)
+  end
+
+  def test_split_avg_stats
+    stats = fit_data_records.partition_stats_for_fields("distance", 1600, ["heart_rate", "cadence", "enhanced_speed", "power"])
+    expected = [[["heart_rate", [126.70692717584369, "bpm"]],
+      ["cadence", [91.2980251346499, "rpm"]],
+      ["enhanced_speed", [2.7501938958707344, "m/s"]],
+      ["power", [205.21824686940965, "watts"]]],
+      [["heart_rate", [124.12641509433962, "bpm"]],
+        ["cadence", [92.08490566037736, "rpm"]],
+        ["enhanced_speed", [3.0754490566037753, "m/s"]],
+        ["power", [208.6867924528302, "watts"]]],
+      [["heart_rate", [128.69296740994855, "bpm"]],
+        ["cadence", [89.79381443298969, "rpm"]],
+        ["enhanced_speed", [2.770613402061855, "m/s"]],
+        ["power", [222.10824742268042, "watts"]]],
+      [["heart_rate", [131.4804753820034, "bpm"]],
+        ["cadence", [85.44482173174873, "rpm"]],
+        ["enhanced_speed", [2.6420152801358245, "m/s"]],
+        ["power", [201.68081494057725, "watts"]]],
+      [["heart_rate", [134.13513513513513, "bpm"]],
+        ["cadence", [91.08108108108108, "rpm"]],
+        ["enhanced_speed", [3.1840675675675674, "m/s"]],
+        ["power", [223.8108108108108, "watts"]]]]
+    assert_equal(expected, stats)
   end
 end
